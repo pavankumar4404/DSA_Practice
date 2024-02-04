36030962800e80dd54a95f1061922b4dad2a9da1
pavankumar4404 <pavan4404kpk@gmail.com>
2024-02-04 23:30:59 +0530

    dp on grids

---
+++ dp_grid_unique_paths.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int i,int j,vector<vector<int>>&dp){
+	if(i==0 && j==0){
+		return 1;
+	}
+	if(i<0 || j<0) return 0;
+	if(dp[i][j]!=-1) return dp[i][j];
+	int up=solve(i-1,j,dp);
+	int left=solve(i,j-1,dp);
+	return dp[i][j]=up+left;
+}
+int uniquePaths(int m, int n) {
+	// Write your code here.
+	vector<vector<int>> dp(m,vector<int>(n,-1));
+	// return solve(m-1,n-1,dp);
+	// dp[0][0]=1;
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			if(i==0 && j==0) dp[i][j]=1;
+			else{
+				int up=0;
+				int left=0;
+				if(i>0) up=dp[i-1][j];
+				if(j>0)	left=dp[i][j-1];
+				dp[i][j]=up+left;
+			}
+		}
+	}
+	return dp[m-1][n-1];
+}
+
+int main() {
+    int m = 2;  // Number of rows
+    int n = 2;  // Number of columns
+
+    int result = uniquePaths(m, n);
+
+    cout << "Number of unique paths in a " << m << "x" << n << " grid: " << result << endl;
+
+    return 0;
+}
