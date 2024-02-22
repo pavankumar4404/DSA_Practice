6f137a39a10ac04cd4a502705280db19cd5b3a99
pavankumar4404 <pavan4404kpk@gmail.com>
2024-02-22 19:29:24 +0530

    cp-31

---
+++ A_Halloumi_Boxes.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+// Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+
+#define yes cout<<"YES"<<'\n'
+#define no cout<<"NO"<<'\n'
+#define inp(x) int x; cin>>x;
+#define br cout<<'\n'
+
+
+// Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+
+void solve(){
+    
+    inp(n)
+    inp(k)
+    int arr[n];
+    for(int i=0; i<n;i++){
+        cin>>arr[i];
+    }
+    if(k==1){
+        for(int i=1;i<n;i++){
+            if(arr[i]<arr[i-1]){
+                no;
+                return;
+            }
+        }
+        yes;
+        
+    }
+    else{
+        yes;
+
+    }
+
+}
+
+int32_t main()
+{
+    fastio()
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
